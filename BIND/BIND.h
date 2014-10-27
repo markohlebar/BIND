b1f5e7b602fa8d70bd5058d0903e915f2fa52eda
Marko Hlebar <marko.hlebar@gmail.com>
2014-10-27 17:43:19 +0000

    Initial Commit

---
+++ BIND/BIND.h
@@ -0,0 +1,13 @@
+//
+//  BIND.h
+//  BIND
+//
+//  Created by Marko Hlebar on 27/10/2014.
+//  Copyright (c) 2014 Marko Hlebar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BIND : NSObject
+
+@end
---
+++ BIND/BIND.m
@@ -0,0 +1,13 @@
+//
+//  BIND.m
+//  BIND
+//
+//  Created by Marko Hlebar on 27/10/2014.
+//  Copyright (c) 2014 Marko Hlebar. All rights reserved.
+//
+
+#import "BIND.h"
+
+@implementation BIND
+
+@end
