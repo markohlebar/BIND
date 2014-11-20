b139fa132186543f90de3b43bc8f8a54f2e812e7
Marko Hlebar <marko.hlebar@gmail.com>
2014-11-20 18:25:33 +0000

    Created a category on UIButton

---
+++ BIND/Categories/UIButton+BNDBinding.h
@@ -0,0 +1,13 @@
+//
+//  UIButton+BNDBinding.h
+//  BIND
+//
+//  Created by Marko Hlebar on 20/11/2014.
+//  Copyright (c) 2014 Marko Hlebar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIButton (BNDBinding)
+
+@end
---
+++ BIND/Categories/UIButton+BNDBinding.m
@@ -0,0 +1,13 @@
+//
+//  UIButton+BNDBinding.m
+//  BIND
+//
+//  Created by Marko Hlebar on 20/11/2014.
+//  Copyright (c) 2014 Marko Hlebar. All rights reserved.
+//
+
+#import "UIButton+BNDBinding.h"
+
+@implementation UIButton (BNDBinding)
+
+@end
