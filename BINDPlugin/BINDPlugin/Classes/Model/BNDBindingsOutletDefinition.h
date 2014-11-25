b9dc08b904c8d4ac459122c06ec1ba133538454f
Marko Hlebar <marko.hlebar@gmail.com>
2014-11-25 21:16:36 +0000

    Created bindings outlet definition

---
+++ BINDPlugin/BINDPlugin/Classes/Model/BNDBindingsOutletDefinition.h
@@ -0,0 +1,13 @@
+//
+//  BNDBindingsOutletDefinition.h
+//  BINDPlugin
+//
+//  Created by Marko Hlebar on 25/11/2014.
+//  Copyright (c) 2014 Marko Hlebar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNDBindingsOutletDefinition : NSObject
+
+@end
---
+++ BINDPlugin/BINDPlugin/Classes/Model/BNDBindingsOutletDefinition.m
@@ -0,0 +1,13 @@
+//
+//  BNDBindingsOutletDefinition.m
+//  BINDPlugin
+//
+//  Created by Marko Hlebar on 25/11/2014.
+//  Copyright (c) 2014 Marko Hlebar. All rights reserved.
+//
+
+#import "BNDBindingsOutletDefinition.h"
+
+@implementation BNDBindingsOutletDefinition
+
+@end
