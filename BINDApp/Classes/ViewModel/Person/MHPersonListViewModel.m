604a451ab6141302f885a41182313c7e49786700
Marko Hlebar <marko.hlebar@gmail.com>
2015-05-18 08:04:02 +0100

    Some MHPersonListViewModel changes.

--- BINDApp/Classes/ViewModel/Person/MHPersonListViewModel.m
+++ BINDApp/Classes/ViewModel/Person/MHPersonListViewModel.m
@@ -11,17 +11,65 @@
 #import "MHPersonCreator.h"
 #import "MHAddPersonCommand.h"
 #import "MHPersonListViewModelTransformer.h"
+#import "BNDTableSectionViewModel.h"
 
 @implementation MHPersonListViewModel
 BINDINGS(MHPersonCreator,
-         BINDModelT(personae, ~>, subViewModels, MHPersonListViewModelTransformer),
-         nil)
+         
+         BINDCreateRows(personae, ->, children),
+         nil
+         );
 
 - (id <BNDCommand> )createPersonCommand {
     if (!_createPersonCommand) {
         _createPersonCommand = [MHAddPersonCommand commandWithCreator:self.model];
     }
     return _createPersonCommand;
+}
+
+- (instancetype)initWithModel:(id)model
+{
+    self = [super initWithModel:model];
+    if (self) {
+        [self observeModel];
+    }
+    return self;
+}
+
+- (void)observeModel {
+    __weak typeof(self) weakSelf = self;
+    MHPersonCreator *creator = self.model;
+    [BINDO(creator, personae) observe:^(id observable, id value, NSDictionary *observationInfo){
+        [weakSelf addRowForObservationInfo:observationInfo];
+    }];
+}
+
+- (void)addRowForObservationInfo:(NSDictionary *)observationInfo {
+    NSIndexSet *indexSet = observationInfo[NSKeyValueChangeIndexesKey];
+    MHPerson *model = observationInfo[NSKeyValueChangeNewKey];
+    NSKeyValueChange kind = [observationInfo[NSKeyValueChangeKindKey] integerValue];
+    
+    switch (kind) {
+        case NSKeyValueChangeInsertion:
+
+            break;
+        case NSKeyValueChangeRemoval:
+ 
+            break;
+
+        default:
+            break;
+    }
+                             
+}
+
+- (BNDTableSectionViewModel *)sectionViewModel {
+    BNDTableSectionViewModel *section = [self.subViewModels firstObject];
+    if (!section) {
+        section = [BNDTableSectionViewModel viewModelWithModel:nil];
+        [self addSubViewModel:section];
+    }
+    return section;
 }
 
 @end
